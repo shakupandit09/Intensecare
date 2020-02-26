3aa0de2e86f56fdb50b617cf4119864cdddcb812
Sunil Sahu <Sunilsahu6618@gmail.com>
2020-02-26 14:53:36 +0530

    TempController

---
+++ IntenseCare/Areas/Admin/Controllers/TempController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntenseCare.Areas.Admin.Controllers
+{
+    public class TempController : Controller
+    {
+        // GET: Admin/Temp
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
